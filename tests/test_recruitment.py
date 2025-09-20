35dbf9fbd5a0869ad60a8917799b1463db1791f4
tuanh <dangtuanh89@gmail.com>
2025-09-20 20:21:16 +0700

    updated

--- tests/test_recruitment.py
+++ tests/test_recruitment.py
@@ -2,8 +2,9 @@ from base.base_test import BaseTest
 from pages.login_page import LoginPage
 from pages.dashboard_page import DashboardPage
 from pages.recruitment_page import RecruitmentPage
-
+import pytest
 class TestRecruitmentPage(BaseTest):
+    @pytest.mark.smoke
     def test_navigate_to_recruitement_page(self):
         login_page = LoginPage(self.driver)
         login_page.login('Admin', 'admin123')
