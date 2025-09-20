f9dd5a3a8d8d41bf3c4cb928615994a43dfd4f5b
tuanh <dangtuanh89@gmail.com>
2025-09-20 21:05:32 +0700

    updated

--- base/base_test.py
+++ base/base_test.py
@@ -1,17 +1,23 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from utils.config_reader import ConfigReader
 class BaseTest:
     @pytest.fixture(scope='class', autouse= True)
     def setup_driver(self, request):
         chrome_options = webdriver.ChromeOptions()
         # Chrome options cho headless mode trên Ubuntu/Linux
-        chrome_options.add_argument('--headless')
+        #chrome_options.add_argument('--headless')
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--disable-dev-shm-usage')
-        chrome_options.add_argument('--disable-gpu')
+
         self.driver = webdriver.Chrome(options=chrome_options)
-        self.driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+        self.driver.maximize_window()
+        
+        base_url = ConfigReader.get_base_url()
+        if base_url:
+            self.driver.get(base_url)
+            
         request.cls.driver = self.driver
         yield
         self.driver.quit()
--- tests/test_login.py
+++ tests/test_login.py
@@ -2,11 +2,13 @@ from base.base_test import BaseTest
 from pages.login_page import LoginPage
 from pages.dashboard_page import DashboardPage
 import pytest
+from utils.config_reader import ConfigReader
 class TestLogin(BaseTest):
     @pytest.mark.smoke
     def test_login(self):
+        
         login_page = LoginPage(self.driver)
-        login_page.login('Admin', 'admin123')
+        login_page.login(ConfigReader.get_username(), ConfigReader.get_password())
         
         dashboard_page = DashboardPage(self.driver)
         assert dashboard_page.confirm_navigate_to_dashboard_page(), "Navigated to Dashboard page failed"
